4f9f371a5fce9cb1218ec65b4a2b1e4e9da4c21e
Shawky Elshazly <Shawkyelshazly2@gmail.com>
2023-09-28 07:37:10 +0300

    adjusted database schema and created basic users

--- prisma/index.js
+++ prisma/index.js
@@ -2,7 +2,28 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
-async function main() {}
+async function main() {
+	[
+		{
+			username: "shawky",
+			password: "$2a$10$kRP1OrHoAgY2fb7wSs0PhOl.UEFba1ceZCAGTOOoCzhKeTO1gzIQq",
+			employeeId: 2,
+		},
+		{
+			username: "abassy",
+			password: "$2a$10$kRP1OrHoAgY2fb7wSs0PhOl.UEFba1ceZCAGTOOoCzhKeTO1gzIQq",
+			employeeId: 1,
+		},
+	].forEach(async (employee) => {
+		await prisma.loginDetails.create({
+			data: {
+				username: employee.username,
+				password: employee.password,
+				employeeId: employee.employeeId,
+			},
+		});
+	});
+}
 
 main()
 	.then(async () => {
@@ -14,4 +35,4 @@ main()
 		process.exit(1);
 	});
 
-export default prisma;
+// export default prisma;
---
+++ prisma/migrations/20230928041447_new_db/migration.sql
@@ -0,0 +1,83 @@
+-- CreateEnum
+CREATE TYPE "Position" AS ENUM ('representative', 'teamLead', 'supervisor', 'qualityAnalyst', 'trainer', 'hr', 'rtm', 'it');
+
+-- CreateEnum
+CREATE TYPE "VacationRequestStatus" AS ENUM ('pending', 'approved', 'denied');
+
+-- CreateEnum
+CREATE TYPE "VacationReason" AS ENUM ('business_trip', 'annual', 'casual', 'sick');
+
+-- CreateTable
+CREATE TABLE "Department" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "parentId" INTEGER,
+
+    CONSTRAINT "Department_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "LoginDetails" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "employeeId" INTEGER NOT NULL,
+
+    CONSTRAINT "LoginDetails_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Employee" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "position" "Position" NOT NULL DEFAULT 'representative',
+    "departmentId" INTEGER NOT NULL,
+    "managerId" INTEGER,
+    "vacationBalance" INTEGER NOT NULL DEFAULT 21,
+
+    CONSTRAINT "Employee_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "VacationRequest" (
+    "id" SERIAL NOT NULL,
+    "from" TIMESTAMP(3) NOT NULL,
+    "to" TIMESTAMP(3) NOT NULL,
+    "reason" "VacationReason" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "approvalStatus" "VacationRequestStatus" NOT NULL DEFAULT 'pending',
+    "employeeId" INTEGER NOT NULL,
+    "approvedBy" INTEGER,
+
+    CONSTRAINT "VacationRequest_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "LoginDetails_username_key" ON "LoginDetails"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "LoginDetails_employeeId_key" ON "LoginDetails"("employeeId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Employee_email_key" ON "Employee"("email");
+
+-- AddForeignKey
+ALTER TABLE "Department" ADD CONSTRAINT "Department_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Department"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "LoginDetails" ADD CONSTRAINT "LoginDetails_employeeId_fkey" FOREIGN KEY ("employeeId") REFERENCES "Employee"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Employee" ADD CONSTRAINT "Employee_departmentId_fkey" FOREIGN KEY ("departmentId") REFERENCES "Department"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Employee" ADD CONSTRAINT "Employee_managerId_fkey" FOREIGN KEY ("managerId") REFERENCES "Employee"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "VacationRequest" ADD CONSTRAINT "VacationRequest_employeeId_fkey" FOREIGN KEY ("employeeId") REFERENCES "Employee"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "VacationRequest" ADD CONSTRAINT "VacationRequest_approvedBy_fkey" FOREIGN KEY ("approvedBy") REFERENCES "Employee"("id") ON DELETE SET NULL ON UPDATE CASCADE;
